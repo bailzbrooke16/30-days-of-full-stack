a37f2f7475794eeeb02cb86e9e11605567043156
Bailey.Brooke <79739001+bailzbrooke16@users.noreply.github.com>
2024-05-08 08:58:23 +0200

    change to playbook not tutorial

--- 30-days-of-full-stack/docusaurus.config.ts
+++ 30-days-of-full-stack/docusaurus.config.ts
@@ -61,7 +61,7 @@ const config: Config = {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Playbook',
         },
         {
           href: 'https://github.com/bailzbrooke16/30-days-of-full-stack',
